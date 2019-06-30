5d470f59c7ba7ab8ad37259dfdb08c3dd4e5eb7e
neosilviu <neosilviu@gmail.com>
2019-06-30 20:45:17 +0300

    Update entrypoint.sh

--- sources/voltronic-mqtt/entrypoint.sh
+++ sources/voltronic-mqtt/entrypoint.sh
@@ -3,10 +3,10 @@ export TERM=xterm
 
 # Init the mqtt server for the first time, then every 5 minutes
 # This will re-create the auto-created topics in the MQTT server if HA is restarted...
-watch -n 300 /opt/mqtt-init.sh > /dev/null 2>&1 &
+watch -n 300 /opt/voltronic-mqtt/mqtt-init.sh > /dev/null 2>&1 &
 
 # Run the MQTT Subscriber process in the background (so that way we can change the configuration on the inverter from home assistant)
-/opt/mqtt-subscriber.sh &
+/opt/voltronic-mqtt/mqtt-subscriber.sh &
 
 # execute exactly every 30 seconds...
-watch -n 30 /opt/mqtt-push.sh > /dev/null 2>&1
+watch -n 30 /opt/voltronic-mqtt/mqtt-push.sh > /dev/null 2>&1
